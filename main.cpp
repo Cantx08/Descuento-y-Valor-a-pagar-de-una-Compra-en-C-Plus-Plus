0abdf0f1e7ab33e48702139ed95b8dfeeb55cdb9
Cantx08 <edwin.cantuna@epn.edu.ec>
2024-03-22 12:30:08 -0500

    Cambios

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     cout << "11 Programa que obtiene el descuento y el valor a pagar de una compra" << endl;
     int n;
     double p, descuento, valorAPagar;
+    string producto;
     // Ingresar el número de Unidades y el precio unitario del producto comprado
     // Las cantidades deben ser mayor a 0.
+    cout << "Ingrese el nombre del producto: ";
+    getline(cin, producto);
+    fflush(stdin);
     cout << "Ingrese el numero de unidades que desea adquirir: ";
     cin >> n;
     cout << "Ingrese el precio unitario del producto: ";
@@ -17,9 +22,11 @@ int main() {
     descuento = 0.1 * n * p;
     // Calcular el valor total de la compra
     valorAPagar = 0.9 * n * p;
+    cout << endl;
 
     // Mostrar el resultado en consola.
-    cout << "El descuento es de " << descuento << " USD." << endl;
-    cout << "La compra se hizo por el monto de " << valorAPagar << " USD.";
+    cout << "Producto: " << producto << endl;
+    cout << "Descuento: " << descuento << " USD." << endl;
+    cout << "Total a pagar: " << valorAPagar << " USD.";
     return 0;
 }
